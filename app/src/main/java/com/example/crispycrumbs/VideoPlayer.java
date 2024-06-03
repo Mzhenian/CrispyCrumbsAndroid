68814c3ca3758967f2ec6e0b7e8e6ed9f684ffa1
DolevMonajem <oren412@gmail.com>
2024-06-03 21:30:50 +0300

    Did some work on login, cleaned drawable folder

---
+++ app/src/main/java/com/example/crispycrumbs/VideoPlayer.java
@@ -0,0 +1,23 @@
+package com.example.crispycrumbs;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class VideoPlayer extends  Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.video_player);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.video_player), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+}

c32260c2abc811935683d6de9252756d2596e9ce
DolevMonajem <oren412@gmail.com>
2024-06-05 16:10:56 +0300

    recycler view working

--- app/src/main/java/com/example/crispycrumbs/VideoPlayer.java
+++ app/src/main/java/com/example/crispycrumbs/VideoPlayer.java
@@ -3,20 +3,28 @@ package com.example.crispycrumbs;
 import android.app.Activity;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class VideoPlayer extends  Activity {
+public class VideoPlayer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
         setContentView(R.layout.video_player);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.comment_fragment_container, new CommentSection_Fragment())
+                    .commit();
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.video_player), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
 
